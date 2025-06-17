afc04e4a30d1c0e4a03bb1939f05b8292f735673
Athersh-SR <athersh946@gmail.com>
2025-06-17 10:10:34 +0530

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,44 @@
             navbar.classList.remove('compressed');
         }
     });
+function animateCounter(counter) {
+  const target = +counter.getAttribute("data-target");
+  let current = 0;
+  const speed = 100;
+  const increment = target / speed;
+
+  function update() {
+    current += increment;
+    if (current < target) {
+      counter.innerText = Math.ceil(current);
+      requestAnimationFrame(update);
+    } else {
+      counter.innerText = target;
+    }
+  }
+
+  update();
+}
+
+const counters = document.querySelectorAll(".stat");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        animateCounter(entry.target);
+      } else {
+        entry.target.innerText = "0";
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+counters.forEach((counter) => {
+  observer.observe(counter);
+});
+
+
